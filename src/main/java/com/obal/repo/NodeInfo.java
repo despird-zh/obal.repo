7dccf2240c8213b04737202ae205291821f06f63
despird-zh <despird.zhang@yahoo.com>
2015-01-03 11:28:40 +0800

    op

--- src/main/java/com/obal/repo/NodeInfo.java
+++ src/main/java/com/obal/repo/NodeInfo.java
@@ -2,16 +2,23 @@ package com.obal.repo;
 
 import java.util.List;
 
+import com.obal.core.accessor.AccessControlEntry;
 import com.obal.core.security.EntryAcl;
 
-public class NodeInfo {
+public class NodeInfo extends AccessControlEntry{
 	
+	private static final long serialVersionUID = 1L;
 	private String nodeName = null;
-	
-	
+		
 	public NodeInfo(String nodeName){
 		
+		super("",null);
 		this.nodeName = nodeName;
+	}
+	
+	public NodeInfo(String entityName, String key){
+		
+		super(entityName,key);
 	}
 	
 	public String getNodeName(){
