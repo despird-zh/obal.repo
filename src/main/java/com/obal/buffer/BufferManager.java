a932c317a92b59fd31dc910bcb0bc3796db0b851
despird-zh <despird.zhang@yahoo.com>
2015-01-15 22:23:07 +0800

    op

--- src/main/java/com/obal/buffer/BufferManager.java
+++ src/main/java/com/obal/buffer/BufferManager.java
@@ -1,7 +1,9 @@
 package com.obal.buffer;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import com.lmax.disruptor.EventFactory;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.obal.disruptor.RingEvent;
 
@@ -11,11 +13,20 @@ public class BufferManager {
 	private ExecutorService executor = null;
 
 	/** the disruptor instance */
-	private Disruptor<RingEvent> disruptor = null;
+	private Disruptor<BufferEvent> disruptor = null;
 
 	public void initial(){
 		
-		
+		// Executor that will be used to construct new threads for consumers
+		this.executor = Executors.newCachedThreadPool();
+		// Specify the size of the ring buffer, must be power of 2.
+		int bufferSize = 1024;
+		BufferFactory eventbuilder = new BufferFactory(1l);
+		// Construct the Disruptor
+		disruptor = new Disruptor<BufferEvent>(eventbuilder, bufferSize, executor);
+		BufferHandler handler = new BufferHandler();
+		// Connect the handler
+		disruptor.handleEventsWith(handler);
 		
 	}
 }
